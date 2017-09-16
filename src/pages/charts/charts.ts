fe9174ad28483e610398bb53c5b13de7a0e21143
Reece W Griffith <rgriff43@jhu.edu>
2017-09-16 01:22:31 -0400

    UI with tabs

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the ChartsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-charts',
+  templateUrl: 'charts.html',
 })
-export class HomePage {
+export class ChartsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad ChartsPage');
   }
 
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the SettingsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-settings',
+  templateUrl: 'settings.html',
 })
-export class HomePage {
+export class SettingsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad SettingsPage');
   }
 
 }
