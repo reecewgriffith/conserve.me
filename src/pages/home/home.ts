290295fbd1e7ea3d9a91f77fff64074619b550de
Reece W Griffith <rgriff43@jhu.edu>
2017-09-16 10:58:44 -0400

    profile pics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the HomePage page.
@@ -14,11 +14,46 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
 
+  presentAlert100pts() {
+    let alert = this.alertCtrl.create({
+      title: 'Gained 100 Green Karma!',
+      subTitle: 'Great Work!',
+      buttons: ['Okay!']
+    });
+    alert.present();
+  }
+
+  presentAlert500pts() {
+    let alert = this.alertCtrl.create({
+      title: 'Gained 500 Green Karma!',
+      subTitle: 'Great Work!',
+      buttons: ['Okay!']
+    });
+    alert.present();
+  }
+
+  presentAlert1kpts() {
+    let alert = this.alertCtrl.create({
+      title: 'Gained 1k Green Karma!',
+      subTitle: 'Great Work!',
+      buttons: ['Okay!']
+    });
+    alert.present();
+  }
+
+  presentAlert200ptsP() {
+    let alert = this.alertCtrl.create({
+      title: 'Gained 200 Pollute Karma!',
+      subTitle: 'We have one planet!',
+      buttons: ['I\'ll do better next time.']
+    });
+    alert.present();
+  }
 }
